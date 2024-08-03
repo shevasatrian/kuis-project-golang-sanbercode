856c0f56fedd8991215ec33620e34f50e056c03c
Sheva Satrian <sheva.sepot@gmail.com>
2024-08-03 07:24:07 +0700

    fix

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
+require github.com/go-gorp/gorp/v3 v3.1.0 // indirect
+
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
@@ -19,6 +21,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -27,6 +30,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/rubenv/sql-migrate v1.7.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
--- main.go
+++ main.go
@@ -2,20 +2,42 @@ package main
 
 import (
 	"book-category-api/cmd/api"
-	"book-category-api/internal/models"
+	database "book-category-api/db"
+	"database/sql"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	DB  *sql.DB
+	err error
 )
 
 func main() {
-	dataSourceName := os.Getenv("DATABASE_URL")
-	if dataSourceName == "" {
-		log.Fatal("DATABASE_URL is not set")
+	err = godotenv.Load("config/.env")
+	if err != nil {
+		panic("Error loading .env file")
 	}
 
-	if err := models.InitDB(); err != nil {
-		log.Fatalf("Could not connect to the database: %v", err)
+	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
+	)
+
+	DB, err = sql.Open("postgres", psqlInfo)
+	defer DB.Close()
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
 	}
+
+	database.DBMigrate(DB)
 
 	router := api.SetupRouter()
 	if err := router.Run(":8080"); err != nil {
